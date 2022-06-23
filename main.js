d924df7abd3539387b19a8691dd88988e3a20a98
Brian Washington <bw49078@gmail.com>
2022-06-22 23:28:27 -0500

    added more colors and h1 displays name of color

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const change = document.getElementById("changeBtn");
+const h1 = document.getElementById("color");
 
 function pickColors() {
-    let colors = ["red", "green", "blue", 'yellow'];
+    let colors = ["Red", "Green", "Blue", "Yellow", "Pink", "Brown", "Black", "Orange", "Purple" ];
     let randomColor = colors[Math. floor(Math. random()*colors. length)];
     document.body.style.backgroundColor = randomColor;
+    return randomColor
 }
 
 change.addEventListener("click", function() {
-    pickColors();
+    pickColors()
+    h1.textContent = "Background Color: " + pickColors();
 })
+
